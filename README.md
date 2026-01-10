0c1797593346c0afd4df005b5243b4d949bc152e
Valentin Ionescu <valiyuneski@gmail.com>
2026-01-10 08:39:56 +0100

    first version

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# flet_key_widget
+# create & activate a virtual environment (optional but recommended)
+uv venv
+source .venv/bin/activate   # macOS/Linux
+
+# install flet
+pip install 'flet[all]==0.80.1' --upgrade
+
+# Install the required packages
+pip install flet keyboard
+
+# install requirements.txt
+pip install -r requirements.txt
+
+# run the app
+python -m flet_key_widget.main
---
+++ main.py
@@ -0,0 +1,36 @@
+import flet as ft
+
+class Body(ft.Container):
+    def __init__(self):
+        super().__init__()
+        self.content = ft.Text("Flet app is running!")
+        self.bgcolor = "white"
+        self.padding = 10
+        self.border_radius = 5
+
+def main(page: ft.Page):
+    page.title = "Flet Key Widget"
+    page.window_width = 240
+    page.window_height = 100
+    page.window_resizable = False
+    page.padding = 0
+    page.bgcolor = ft.Colors.TRANSPARENT
+    page.window.bgcolor = ft.Colors.TRANSPARENT
+    page.window.title_bar_hidden = True
+    page.window.title_bar_buttons_hidden = True
+    page.window.frameless = True
+    page.window.always_on_top = True
+    
+    # For macOS, we need to set the window to be transparent
+    if page.platform == "macos":
+        page.window_transparent = True
+        page.window_skip_task_bar = True
+        page.window_focused = True
+    
+    # Add a simple text control to verify the app is working
+    page.add(
+        Body()
+    )
+
+if __name__ == "__main__":
+    ft.app(target=main)
